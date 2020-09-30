f30a3a56fe61b3f2cf8f39efa09c56250e01b25a
zeki47 <zeki.kcln@hotmail.com>
2020-09-30 23:41:42 +0300

    i have updated the three lines

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,5 +1,7 @@
 public class HelloTest {
     public static void main(String[] args) {
-        System.out.println("Hello Class");
+        System.out.println("Hello Class EU3");
+        //more changes are coming
+        //i am ready to commit this
     }
 }
