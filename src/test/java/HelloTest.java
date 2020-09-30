c09881ccba55b0171ca5c521246d5a10bc2c878f
zeki47 <zeki.kcln@hotmail.com>
2020-09-30 23:45:03 +0300

    EU3 is doing great!!

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -3,5 +3,8 @@ public class HelloTest {
         System.out.println("Hello Class EU3");
         //more changes are coming
         //i am ready to commit this
+
+        //EU3 is awesome!!!
+        //EU3 IS LEARNING HOW TO USE GIT FROM INTELLIJ
     }
 }
